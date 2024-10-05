c8c6e8207e9bae58408fdce423855b517a58b2fa
kishore <skill2automate@gmail.com>
2024-10-05 10:08:29 +0530

    first commit

---
+++ project1234/src/test/java/tests/TestSuite1.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuite1 {
+
+	@Test
+	public void test01() {
+		System.out.println("test01");
+	}
+	@Test
+	public void test02() {
+		System.out.println("test02");
+	}
+
+}
